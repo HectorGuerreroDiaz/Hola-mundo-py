b710d6b49e2179332b7ec34f32e215ac7d5789c1
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2020-09-23 22:26:18 -0500

    segunda version

--- primero.py
+++ primero.py
@@ -1,5 +1,11 @@
+def suma(a, b):
+    return a + b
+
 a = input("Edad: ")
 name = "Michael"
 print("hola mundo")
 print(a)
 20
+b = int(input("dime el primer numero: "))
+c = int(input("dime el segundo numero: "))
+print(suma(b,c))
